97a43ee5eb8d20b90d52ed94fd830c3c5ede7086
MacKeil <zack.mackeil@gmail.com>
2017-05-02 12:27:06 -0400

    fixed the server.js file to serve something when there is no specific request

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
-var server = http.createServer(function(q,s){
-	if(q.url.indexOf('.html') != -1){
-		fs.readFile(__dirname + 'main.html', function(e,d){
+var server = http.createServer(function (q,s) {
+	if(q.url.indexOf('.html') != -1 || q.url == '/'){
+		fs.readFile('main.html', function (e,d) {
 				if(e){
 				console.log(e);
 				}
@@ -12,7 +12,7 @@ var server = http.createServer(function(q,s){
 				});
 	}
 	if(q.url.indexOf('.css') != -1){
-		fs.readFile(__dirname + 'main.css', function(e,d){
+		fs.readFile('main.css', function (e,d) {
 				if(e){console.log(e);}
 				s.writeHead(200);
 				s.write(d);
